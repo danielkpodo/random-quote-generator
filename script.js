a583c9051f7f81a1b44372ddf81c2d2db7178090
danielkpodo <kpododanielnarh@gmail.com>
2020-09-12 19:45:19 +0000

    added cors-anywhere free proxy server to resolve cors anywhere issue

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-// Get quote
+// Fetch quote from forrismatic
 async function getQuote() {
-  const apiUrl =
-    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  const proxyUrl = `https://cors-anywhere.herokuapp.com/`;
+  const apiUrl = `http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json`;
   try {
-    const response = await fetch(apiUrl);
-    const data = response.json();
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
     console.log(data);
   } catch (error) {
     console.log("Whoops no quote: " + error);
+    getQuote();
   }
 }
 
